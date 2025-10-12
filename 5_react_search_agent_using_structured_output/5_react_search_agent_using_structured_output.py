5e3385d4daa498165234bb10e979eb3c750d0591
Partha Mishra <34281131+mishrapartha@users.noreply.github.com>
2025-10-12 18:59:42 +0530

    5_react_search_agent_using_structured_output
    5_react_search_agent_with_output_parsing_using_structured_output.
    
    We have made the previous code more pythonic using the llm.with_structured_output() method

---
+++ 5_react_search_agent_using_structured_output/5_react_search_agent_using_structured_output.py
@@ -0,0 +1,49 @@
+# Import Required Libraries
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
+# Loading Environment Variables
+load_dotenv()
+
+# Creating a list of tools that the llm can use
+tools = [TavilySearch()]
+
+
+llm = ChatOpenAI(model="gpt-4o")
+structured_llm = llm.with_structured_output(AgentResponse)
+react_prompt = hub.pull("hwchase17/react")
+
+
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions="")
+
+agent = create_react_agent(
+    llm=llm, tools=tools, prompt=react_prompt_with_format_instructions
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+chain = agent_executor | extract_output | structured_llm
+
+
+def main():
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using  langchain in the UAE on linkedin and list their details"
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
