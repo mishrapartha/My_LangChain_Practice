e147a60edb2fe0642344733e901f876ab88a0041
Partha Mishra <34281131+mishrapartha@users.noreply.github.com>
2025-10-16 14:29:11 +0530

    1_Simple_Tool_Calling_using_LangChain
    1_Simple_Tool_Calling_using_LangChain

---
+++ 3_Function_or_Tool_Calling/1_Simple_Tool_Calling_using_LangChain.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import tool
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+
+
+@tool
+def multiply(x: float, y: float) -> float:
+    """Multiply 'x' times 'y'."""
+    return x * y
+
+
+if __name__ == "__main__":
+    print("Hello Tool Calling")
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "you're a helpful assistant"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+
+    tools = [TavilySearch(), multiply]
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    # llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=0)
+
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+    res = agent_executor.invoke(
+        {
+            "input": "what is the weather in dubai right now? compare it with Bangalore, output should in in celsius",
+        }
+    )
+
+    print(res)
